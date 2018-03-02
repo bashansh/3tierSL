aa9b66e17351fcc6b5963f8090ccbb9011ec3a7c
bashansh <shmuel.bashan@gmail.com>
2018-03-02 12:29:51 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,6 +44,8 @@ variable "ssh_public_key" {
   default = ""
   description = "the ssh_public_key"
 }
+
+
 variable "datacenter" {
   default = "dal13"
   description = "the data center to deploy the VLAN."
@@ -110,7 +112,95 @@ resource "ibm_subnet" "apptier_subnet2" {
   vlan_id = "${ibm_network_vlan.vlan_private.id}"
   notes = "portable_private_APP__subnet"
 }
+#------------------------------------
 
+##############################################################################
+# Create the web tier VMs that will be part of the LB service group
+##############################################################################
+resource "ibm_compute_vm_instance" "webtiervm" {
+  count = "${var.vm_count}"
+  os_reference_code = "${var.osrefcode}"
+  hostname = "${format("webtier-%02d", count.index + 1)}"
+  domain = "${var.domain}"
+  datacenter = "${var.datacenter}"
+  file_storage_ids = "${var.webfileid}"
+  network_speed = 10
+  cores = 1
+  memory = 1024
+  local_disk = true
+  ssh_key_ids = ["${ibm_compute_ssh_key.ssh_key.id}"]
+  local_disk = false
+  private_vlan_id = "${var.private_vlan_id}"
+  public_vlan_id = "${var.public_vlan_id}"
+  private_subnet = "${var.private_vlan_subnet1_id}"
+}
+
+##############################################################################
+# Create the APP tier VMs that will be part of the LB service group
+##############################################################################
+resource "ibm_compute_vm_instance" "apptiervm" {
+  count = "${var.vm_count}"
+  os_reference_code = "${var.osrefcode}"
+  hostname = "${format("apptier-%02d", count.index + 1)}"
+  datacenter = "${var.datacenter}"
+  file_storage_ids = ["${var.appfileid}"]
+  block_storage_ids = ["${var.appblockid}"]
+  network_speed = 10
+  cores = 1
+  memory = 1024
+  disks = [25, 10]
+  ssh_key_ids = ["${ibm_compute_ssh_key.ssh_key.id}"]
+  local_disk = false
+  private_vlan_id = "${var.private_vlan_id}"
+  private_subnet = "${var.private_vlan_subnet2_id}"
+# this is where we add the security group #
+}
+
+
+##############################################################################
+# variables
+##############################################################################
+variable domain {
+  description = "domain of the VMs"
+  default = "mybluemix.com"
+}
+variable privatevlanid {
+  description = "private VLAN"
+  default = "123456"
+}
+variable publicvlanid {
+  description = "public VLAN"
+  default = "123456"
+}
+
+variable ssh_public_key {
+  default = "please fill in"
+}
+variable ssh_label {
+  default = "3-tiersshkey"
+}
+variable ssh_notes {
+  default = "this is the 3 tier example ssh key"
+}
+variable node_count {
+  default = "1"
+}
+variable osrefcode {
+  default = "UBUNTU_LATEST"
+}
+variable vm_count {
+  default = "1"
+}
+################################################
+# Outputs
+################################################
+  output "app_ipaddress" {
+  value = "[${ibm_compute_vm_instance.apptiervm.ip_address_id}]"
+}
+  output "web_ipaddress" {
+  value = "[${ibm_compute_vm_instance.webtiervm.ip_address_id}]"
+}
+#----------------------------------------
 
 ##############################################################################
 # Create a local loadbalancer (ToDo:  replace hard coded values)
@@ -150,7 +240,7 @@ resource "ibm_lb_service" "web_lb_service" {
   # Uses HTTP to as a healthcheck
   health_check_type           = "HTTP"
   # Where to send traffic to
-  ip_address_id               = "${element(module.network.web_ipaddress, count.index)}"
+  ip_address_id               = "${element(web_ipaddress, count.index)}"
   # For demonstration purposes; creates an explicit dependency
   depends_on                  = ["ibm_compute_vm_instance.node"]
 }
@@ -167,9 +257,9 @@ resource "ibm_lb_service" "app_lb_service" {
   # Uses HTTP to as a healthcheck
   health_check_type           = "HTTP"
   # Where to send traffic to
-  ip_address_id               = "${element(module.network.app_ipaddress, count.index)}"
+  ip_address_id               = "${element(app_ipaddress, count.index)}"
   # For demonstration purposes; creates an explicit dependency
-  depends_on                  = ["module.network.web_ipaddress", "module.network.app_ipaddress"]
+  depends_on                  = ["web_ipaddress", "app_ipaddress"]
 }
 
 #################################################
